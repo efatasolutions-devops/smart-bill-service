a243f8364969b871f6f164f3786f94771c7d5de1
arifinlenna <arifin@lenna.ai>
2025-05-19 01:05:57 +0700

    update index.go

--- api/index.go
+++ api/index.go
@@ -1,10 +1,17 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 
+	"github.com/arifin2018/splitbill-arifin.git/config"
+	appconfig "github.com/arifin2018/splitbill-arifin.git/config/appConfig"
+	"github.com/arifin2018/splitbill-arifin.git/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 // Handler is the main entry point of the application. Think of it like the main() method
@@ -19,24 +26,30 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func handler() http.HandlerFunc {
 	app := fiber.New()
 
-	app.Get("/v1", func(ctx *fiber.Ctx) error {
-		return ctx.JSON(fiber.Map{
-			"version": "v1",
-		})
-	})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
-	app.Get("/v2", func(ctx *fiber.Ctx) error {
-		return ctx.JSON(fiber.Map{
-			"version": "v2",
-		})
-	})
+	serverShutdown := make(chan struct{})
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.JSON(fiber.Map{
-			"uri":  ctx.Request().URI().String(),
-			"path": ctx.Path(),
-		})
-	})
+	go func() {
+		_ = <-c
+		fmt.Println("Gracefully shutting down...")
+		_ = app.Shutdown()
+		serverShutdown <- struct{}{}
+	}()
+	appconfig.InitApplication()
+	app.Use(cors.New())
+
+	config.Logger(app)
+	routes.Router(app)
+	if err := app.Listen(":3000"); err != nil {
+		panic(err.Error())
+	}
+
+	<-serverShutdown
+
+	config.GeneralLogger.Println("Running cleanup tasks...")
+	fmt.Println("Running cleanup tasks...")
 
 	return adaptor.FiberApp(app)
 }
